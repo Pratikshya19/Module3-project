a9fccd5edbfb555da2aaa7e5063ff665fb6eac6c
github username <github email address>
2019-05-29 13:11:09 -0500

    wenesday

--- pokemon-backend/app/controllers/trainers_controller.rb
+++ pokemon-backend/app/controllers/trainers_controller.rb
@@ -3,15 +3,15 @@ class TrainersController < ApplicationController
 
 
     def index
-        @pokemons = Pokemon.all
+        @trainers = Trainer.all
         render json: @trainers
     
-end
+    end
 
-def show
-  @trainer = Trainer.find_by(:id)  
-    render json: @trainer
-end
+    def show
+    @trainer = Trainer.find_by(:id)  
+        render json: @trainer
+    end
 
 
 
